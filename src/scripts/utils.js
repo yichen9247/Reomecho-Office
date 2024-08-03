c4a4ccfdbb143670036ac373999c4a89efb5ac9d
yichen9247 <865252486@qq.com>
2024-08-03 12:57:57 +0800

    reomecho office

--- src/scripts/utils.js
+++ src/scripts/utils.js
@@ -1,5 +1,9 @@
 
+import developer from "./developer"
 import siteConfig from "../../config/config"
+import DisableDevtool from 'disable-devtool'
 import { useApplicationStore } from "@/stores/application"
+
+if (import.meta.env.MODE === 'production') DisableDevtool(developer);
 
 export default {siteConfig, useApplicationStore}
